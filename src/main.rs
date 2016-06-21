530d3a1e9457d054dbbe2af0cd02b9bd3390aacc
Matt Baker <matthew.reiss.baker@gmail.com>
2016-06-21 03:10:13 -0700

    print stash

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 extern crate git2;
 
+use std::process::Command;
 use git2::Repository;
+use std::str;
 
 fn main() {
-    let repo = match Repository::open('.') {
+    let repo = match Repository::open(".") {
         Ok(repo) => Some(repo),
         Err(e) => None,
     }.unwrap();
@@ -13,12 +15,19 @@ fn main() {
         Err(e) => None,
     }.unwrap();
 
-    let branch = match head.symbolic_target();
+    let branch = head.symbolic_target();
 
     if branch.is_some() {
-        println!("{}", branch.unwrap());
+        println!("symbolic: {}", branch.unwrap());
     }
     else {
-        println!("{}", head.name().unwrap());
+        println!("direct: {}", head.name().unwrap());
     }
+
+    let output = Command::new("git")
+                         .arg("stash list")
+                         .output()
+                         .unwrap();
+    println!("output: {}", str::from_utf8(&output.stdout).unwrap());
+
 }
