1875873deb0bc00a08885aa91ec9d63779a26c76
Matt Baker <matthew.reiss.baker@gmail.com>
2016-06-22 03:02:29 -0700

    split into two commands

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,74 @@
 extern crate git2;
 
-use std::process::Command;
 use git2::Repository;
+
 use std::str;
+use std::env;
+use std::process;
+use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() {
-    let repo = match Repository::open(".") {
-        Ok(repo) => Some(repo),
-        Err(e) => None,
-    }.unwrap();
-
-    let head = match repo.head() {
-        Ok(reference) => Some(reference),
-        Err(e) => None,
-    }.unwrap();
-
-    let branch = head.symbolic_target();
-
-    if branch.is_some() {
-        println!("symbolic: {}", branch.unwrap());
-    }
-    else {
-        println!("direct: {}", head.name().unwrap());
+    if env::args().count() <= 1 {
+        println!("must pass arguments");
+        process::exit(3);
     }
 
-    let output = Command::new("git")
-                         .arg("stash list")
-                         .output()
-                         .unwrap();
-    println!("output: {}", str::from_utf8(&output.stdout).unwrap());
+    let argstring = env::args().nth(1).unwrap();
+    let arg = argstring.as_str();
+    let output = match arg {
+        "branch" => branch(),
+        "stash-depth" => stash_depth().map(|depth| format!("{}", depth)),
+        _ => {
+            println!("unknown argument {}", arg);
+            process::exit(3);
+        }
+    };
 
+    match output {
+        Some(string) => println!("{}", string),
+        None => process::exit(2),
+    }
+}
+
+fn branch() -> Option<String> {
+    let repo = Repository::open(".").ok();
+    repo.and_then(|repo| {
+        repo.head().ok().and_then(|head| {
+            match head.symbolic_target() {
+                Some(branchname) => Some(String::from(branch_from_refname(branchname))),
+                None => Some(String::from(branch_from_refname(head.name().unwrap()))),
+            }
+        })
+    })
+}
+
+fn branch_from_refname(refname: &str) -> &str {
+    refname.split("/").last().unwrap()
+}
+
+fn stash_depth() -> Option<u64> {
+    let f = match File::open(".git/refs/stash") {
+        Ok(file) => file,
+
+        /*
+         * If opening this file fails, check if we're in a git repo at all. If
+         * we are, the stash depth is zero; if we're not, there is no stash
+         * (because there is no git repo).
+         */
+        Err(_) => match Path::new(".git").exists() {
+            true => return Some(0),
+            false => return None,
+        }
+    };
+
+    let reader = BufReader::new(f);
+    let mut stash_size = 0;
+    for _ in reader.lines() {
+        stash_size += 1;
+    }
+
+    Some(stash_size)
 }
